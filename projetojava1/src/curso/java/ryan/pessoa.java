700c378c14a9df2aa6b01bb6d3bbe07dd1997aea
ryanvreis <ryaanvreis@gmail.com>
2023-10-23 17:21:07 -0300

    Adicionando classe pessoa

---
+++ projetojava1/src/curso/java/ryan/pessoa.java
@@ -0,0 +1,48 @@
+package curso.java.ryan;
+
+import java.util.Date;
+
+public class pessoa {
+	private String nome;
+	private String sobrenome;
+	private String CPF;
+	private int Idade;
+	private Date dataDeNascimento;
+	private Date dataCadastro;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSobrenome() {
+		return sobrenome;
+	}
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	public String getCPF() {
+		return CPF;
+	}
+	public void setCPF(String cPF) {
+		CPF = cPF;
+	}
+	public int getIdade() {
+		return Idade;
+	}
+	public void setIdade(int idade) {
+		Idade = idade;
+	}
+	public Date getDataDeNascimento() {
+		return dataDeNascimento;
+	}
+	public void setDataDeNascimento(Date dataDeNascimento) {
+		this.dataDeNascimento = dataDeNascimento;
+	}
+	public Date getDataCadastro() {
+		return dataCadastro;
+	}
+	public void setDataCadastro(Date dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+}
