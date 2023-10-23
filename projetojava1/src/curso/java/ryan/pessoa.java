571b6fcd965234720895dd7fccb938413a364c8c
ryanvreis <ryaanvreis@gmail.com>
2023-10-23 17:31:40 -0300

    Removendo campo de cadastro

--- projetojava1/src/curso/java/ryan/pessoa.java
+++ projetojava1/src/curso/java/ryan/pessoa.java
@@ -8,41 +8,44 @@ public class pessoa {
 	private String CPF;
 	private int Idade;
 	private Date dataDeNascimento;
-	private Date dataCadastro;
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getSobrenome() {
 		return sobrenome;
 	}
+
 	public void setSobrenome(String sobrenome) {
 		this.sobrenome = sobrenome;
 	}
+
 	public String getCPF() {
 		return CPF;
 	}
+
 	public void setCPF(String cPF) {
 		CPF = cPF;
 	}
+
 	public int getIdade() {
 		return Idade;
 	}
+
 	public void setIdade(int idade) {
 		Idade = idade;
 	}
+
 	public Date getDataDeNascimento() {
 		return dataDeNascimento;
 	}
+
 	public void setDataDeNascimento(Date dataDeNascimento) {
 		this.dataDeNascimento = dataDeNascimento;
-	}
-	public Date getDataCadastro() {
-		return dataCadastro;
-	}
-	public void setDataCadastro(Date dataCadastro) {
-		this.dataCadastro = dataCadastro;
 	}
 }
