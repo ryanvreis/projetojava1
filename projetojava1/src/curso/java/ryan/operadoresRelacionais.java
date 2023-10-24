2245e84f22e9f005da888f6b16f7d97ba3598102
ryanvreis <ryaanvreis@gmail.com>
2023-10-24 12:38:39 -0300

    Adicionando a classe operadoresRelacionais

---
+++ projetojava1/src/curso/java/ryan/operadoresRelacionais.java
@@ -0,0 +1,22 @@
+package curso.java.ryan;
+
+public class operadoresRelacionais {
+	public static void main(String[] args) {
+		
+	int nota1 = 7;
+	int nota2 = 6;
+	
+	if(nota1 == nota2) {
+		System.out.println("As notas são iguais.");
+	}
+	else {
+		System.out.println("As notas são diferentes");
+	}
+	if(nota1 > nota2) {
+		System.out.println("A nota1 é maior que a nota2.");
+	}
+	else {
+		System.out.println("A nota 1 é menor que a nota2");
+	}
+}
+}
