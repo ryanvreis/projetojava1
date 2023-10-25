262c841032dd9257d55518d7e695622fe9c0b275
ryanvreis <ryaanvreis@gmail.com>
2023-10-25 15:56:28 -0300

    Criando caixa de confirmação na classe entradaDeDados

--- projetojava1/src/curso/java/ryan/entradaDeDados.java
+++ projetojava1/src/curso/java/ryan/entradaDeDados.java
@@ -7,16 +7,32 @@ public class entradaDeDados {
 	public static void main(String[] args) {
 		String carros = JOptionPane.showInputDialog("Informe a quantidade de carros: ");
 		String pessoas = JOptionPane.showInputDialog("Informe a quantidade de pessoas: ");
-		
-		
+
 		double carroNumero = Double.parseDouble(carros);
 		double pessoaNumero = Double.parseDouble(pessoas);
-		
+
 		int divisao = (int) (carroNumero / pessoaNumero);
+
 		double resto = carroNumero % pessoaNumero;
+
+		int resposta = JOptionPane.showConfirmDialog(null, "Deseja ver o resultado da divisão? ");
+
+		if (resposta == 0) {
+			JOptionPane.showMessageDialog(null, "Divisão para pessoas deu: " + divisao);
+		}
+		else {
+			System.out.println("Não quis ver o resultado!!!");
+		}
 		
+		resposta = JOptionPane.showConfirmDialog(null, "Deseja ver o resto da divisão? ");
 		
-		JOptionPane.showMessageDialog(null, "Divisão para pessoas deu: "+ divisao + " carros e sobrou: " + resto + "carros");
+		if (resposta == 0) {
+			JOptionPane.showMessageDialog(null, "Deseja ver o resto da divisão? " + resto);
+		}
+		else {
+			System.out.println("Não quis ver o resto!!!");
+		}
+
 	}
 
 }
