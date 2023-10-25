a5109505658d4c03f3e8a78c40fd59fe7b2ad5df
ryanvreis <ryaanvreis@gmail.com>
2023-10-25 14:48:10 -0300

    Adicionando classe for

---
+++ projetojava1/src/curso/java/ryan/estruturaFor.java
@@ -0,0 +1,13 @@
+package curso.java.ryan;
+
+public class estruturaFor {
+	public static void main(String args[]) {
+
+		for (int numero = 0; numero <= 10; numero++) {
+			System.out.println("Número atual é: " + numero);
+		}
+		for (int numero2 = 10; numero2 >= 0; numero2--) {
+			System.out.println("Número atual é: " + numero2);
+		}
+	}
+}
