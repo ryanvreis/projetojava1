bfb9ae8f220df86da942eac74d0c468a9ef1f99c
ryanvreis <ryaanvreis@gmail.com>
2023-10-25 15:22:10 -0300

    Adicionando classe mod

---
+++ projetojava1/src/curso/java/ryan/mod.java
@@ -0,0 +1,10 @@
+package curso.java.ryan;
+
+public class mod {
+	public static void main(String[] args) {
+		double num1 = 9;
+		double num2 = 4;
+		double resto = num1 % num2;
+		System.out.println("Sobraram "+ resto + " carros");
+	}
+}
