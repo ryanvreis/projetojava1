52fa1ede278bacf44246f0a0f4327d660342a8e3
ryanvreis <ryaanvreis@gmail.com>
2023-10-25 15:34:34 -0300

    Criando classe entradaDeDados utilizando JOptionPane

---
+++ projetojava1/src/curso/java/ryan/entradaDeDados.java
@@ -0,0 +1,22 @@
+package curso.java.ryan;
+
+import javax.swing.JOptionPane;
+
+public class entradaDeDados {
+
+	public static void main(String[] args) {
+		String carros = JOptionPane.showInputDialog("Informe a quantidade de carros: ");
+		String pessoas = JOptionPane.showInputDialog("Informe a quantidade de pessoas: ");
+		
+		
+		double carroNumero = Double.parseDouble(carros);
+		double pessoaNumero = Double.parseDouble(pessoas);
+		
+		int divisao = (int) (carroNumero / pessoaNumero);
+		double resto = carroNumero % pessoaNumero;
+		
+		
+		JOptionPane.showMessageDialog(null, "Divisão para pessoas deu: "+ divisao + " carros e sobrou: " + resto + "carros");
+	}
+
+}
