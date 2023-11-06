50bfbdf73f4a287efda7897f45212f15c98848e7
ryanvreis <ryaanvreis@gmail.com>
2023-11-06 17:56:32 -0300

    Adicionando as classes for e else

--- projetojava1/src/curso/java/ryan/estruturaFor.java
+++ projetojava1/src/curso/java/ryan/estruturaFor.java
@@ -3,19 +3,18 @@ package curso.java.ryan;
 public class estruturaFor {
 	public static void main(String args[]) {
 		/*
-		// Estrutura de repetição FOR crescente
-		for (int numero = 0; numero <= 10; numero++) {
-			System.out.println("Número atual é: " + numero);
-		}*/
+		 * // Estrutura de repetição FOR crescente for (int numero = 0; numero <= 10;
+		 * numero++) { System.out.println("Número atual é: " + numero); }
+		 */
 		/*
-		// Estrutura de repetição FOR decrescente
-		for (int numero2 = 10; numero2 >= 0; numero2--) {
-			System.out.println("Número atual é: " + numero2);
-		} */
-		    /* Estrutura de repetição FOR com break(parada) for (int i = 0; i <= 7; i++)
-			  { if (i == 7) { System.out.println("Encontrei o numero 7");
-			  System.out.println("Estou parando de executar!!!"); break; } }
-			 */
+		 * // Estrutura de repetição FOR decrescente for (int numero2 = 10; numero2 >=
+		 * 0; numero2--) { System.out.println("Número atual é: " + numero2); }
+		 */
+		/*
+		 * Estrutura de repetição FOR com break(parada) for (int i = 0; i <= 7; i++) {
+		 * if (i == 7) { System.out.println("Encontrei o numero 7");
+		 * System.out.println("Estou parando de executar!!!"); break; } }
+		 */
 		// Estrutura de repetição FOR e continue
 		for (int i = 0; i <= 10; i++) {
 			if (i == 3 || i == 6 || i == 10) {
--- projetojava1/src/curso/java/ryan/iFeELse.java
+++ projetojava1/src/curso/java/ryan/iFeELse.java
@@ -12,21 +12,23 @@ public class iFeELse {
 		int media = (nota1 + nota2 + nota3 + nota4) / 4;
 
 		// Operadores lógicos IF && ELSE
-		/*
-		 * if (media >= 60) { System.out.println("Aluno foi aprovado" + media); } else
-		 * if (media >= 30 && media <= 59) {
-		 * System.out.println("Aluno está em recuperação" + media); } else {
-		 * System.out.println("Aluno foi reprovado" + media); }
-		 */
+
+		if (media >= 60) {
+			System.out.println("Aluno foi aprovado" + media);
+		} else if (media >= 30 && media <= 59) {
+			System.out.println("Aluno está em recuperação" + media);
+		} else {
+			System.out.println("Aluno foi reprovado" + media);
+		}
 
 		// Operadores ternários para micro validações
-		/*
-		 * String saidaResultado = media >= 60 ? "Aluno aprovado" : (media >= 30 &&
-		 * media <= 59) ? "Aluno em recuperação" : "Aluno reprovado";
-		 * System.out.println(saidaResultado);
-		 */
-		//Operações lógicas aninhadas: São operações dentro de operações
-		/*
+
+		String saidaResultado = media >= 60 ? "Aluno aprovado"
+				: (media >= 30 && media <= 59) ? "Aluno em recuperação" : "Aluno reprovado";
+		System.out.println(saidaResultado);
+
+		// Operações lógicas aninhadas: São operações dentro de operações
+
 		if (media >= 60) {
 			if (media >= 70) {
 				if (media > 90) {
@@ -40,6 +42,6 @@ public class iFeELse {
 		} else {
 			System.out.println("Aluno está reprovado " + media);
 		}
-		*/
+
 	}
 }
